ee281cf746c3d24d690d520ff497896e1dec4a68
Yunus Kocaman <yunuskocaman@brandeis.edu>
2025-02-06 18:46:12 -0500

    final commit before vercel deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" sizes="any" />
+      </head>
       <body
         className={`${oswald.className} ${geistMono.variable} antialiased`}
       >
