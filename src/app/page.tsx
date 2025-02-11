cb1e989669fb6d3d3c0de80750f758aad06af66d
Yunus Kocaman <yunuskocaman@brandeis.edu>
2025-02-10 19:36:25 -0500

    Added copyright

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,26 +10,31 @@ import Contact from "./pages/contact";
 
 export default function Home() {
   return (
-    <main className="min-h-[50vh] md:min-h-[200vh]">
-      <Navbar/>
-      <div id="top">
-        <ScrollAnimationWrapper>
-          <Header/>
-        </ScrollAnimationWrapper>
-      </div>
-      <div id="about" className="scroll-mt-32">
-        <ScrollAnimationWrapper>
-          <About/>
-        </ScrollAnimationWrapper>
-      </div>
-      <div id="projects" className="scroll-mt-32">
-        <Projects/>
-      </div>
-      <div id="contact" className="scroll-mt-32">
-        <ScrollAnimationWrapper>
-          <Contact/>
-        </ScrollAnimationWrapper>
-      </div>
-    </main>
+    <div className="flex flex-col min-h-screen">
+      <main className="min-h-[50vh] md:min-h-[200vh]">
+        <Navbar/>
+        <div id="top">
+          <ScrollAnimationWrapper>
+            <Header/>
+          </ScrollAnimationWrapper>
+        </div>
+        <div id="about" className="scroll-mt-32">
+          <ScrollAnimationWrapper>
+            <About/>
+          </ScrollAnimationWrapper>
+        </div>
+        <div id="projects" className="scroll-mt-32">
+          <Projects/>
+        </div>
+        <div id="contact" className="scroll-mt-32">
+          <ScrollAnimationWrapper>
+            <Contact/>
+          </ScrollAnimationWrapper>
+        </div>
+      </main>
+      <footer className="py-4 text-center text-gray-600 text-sm mt-auto font-semibold">
+        © {new Date().getFullYear()} Yunus Kocaman. All Rights Reserved.
+      </footer>
+    </div>
   );
 }
